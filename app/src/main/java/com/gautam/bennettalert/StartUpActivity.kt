9b22606ad2930c6744e5e24b32ac9c47ceaefc20
gautambitious <mvgt.gautam@gmail.com>
2019-07-26 01:21:50 +0530

    AlertApp: MainActivity Layout Complete, StartActivity Add

---
+++ app/src/main/java/com/gautam/bennettalert/StartUpActivity.kt
@@ -0,0 +1,12 @@
+package com.gautam.bennettalert
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_up)
+    }
+}
