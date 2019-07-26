2a341670bd6cbcf635d1e7e31e186fc8f40e9e78
gautambitious <mvgt.gautam@gmail.com>
2019-07-26 22:28:52 +0530

    Bennett Alert: Firebase connect + gitignore update

--- app/src/main/java/com/gautam/bennettalert/StartUpActivity.kt
+++ app/src/main/java/com/gautam/bennettalert/StartUpActivity.kt
@@ -1,7 +1,12 @@
 package com.gautam.bennettalert
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 
 class StartUpActivity : AppCompatActivity() {
 
@@ -9,4 +14,6 @@ class StartUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start_up)
     }
+
+
 }
