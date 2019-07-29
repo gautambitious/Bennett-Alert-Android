05e017504d97431a34e2168e77bdcb3327881920
gautambitious <mvgt.gautam@gmail.com>
2019-07-30 01:05:19 +0530

    Layout Update

--- app/src/main/java/com/gautam/bennettalert/NewAlertActivity.kt
+++ app/src/main/java/com/gautam/bennettalert/NewAlertActivity.kt
@@ -2,11 +2,25 @@ package com.gautam.bennettalert
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_new_alert.*
 
 class NewAlertActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_alert)
+
+        // Drop Down Menu Adapter
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.priority_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            prioritySpinner.adapter = adapter
+        }
     }
 }
