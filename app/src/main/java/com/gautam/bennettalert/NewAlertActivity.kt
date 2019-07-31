fb2ab9e67d0e69e7155c1fa8c4a91ecc974888ee
gautambitious <mvgt.gautam@gmail.com>
2019-07-31 23:21:32 +0530

    BennettAlert: Work Save Day

--- app/src/main/java/com/gautam/bennettalert/NewAlertActivity.kt
+++ app/src/main/java/com/gautam/bennettalert/NewAlertActivity.kt
@@ -3,6 +3,7 @@ package com.gautam.bennettalert
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_new_alert.*
 import org.jetbrains.anko.toast
--- app/src/main/java/com/gautam/bennettalert/NewQuery Database/Alert.kt
+++ app/src/main/java/com/gautam/bennettalert/NewQuery Database/Alert.kt
@@ -5,7 +5,7 @@ import androidx.room.*
 @Entity
 data class Alert(
     @PrimaryKey(autoGenerate = true)
-    val queryId: Int,
+    val queryId: Long,
     val queryDetails: String,
     val queryAddress: String,
     val queryPriorityHigh: Boolean
